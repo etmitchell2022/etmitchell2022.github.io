2821a688f9306a58368ff8dcd432e1299b00eade
Evan Mitchell <etmitchell@bsu.edu>
2021-03-06 00:37:58 -0500

    Update skills

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\evty2\\Desktop\\Side Projects\\cvSite\\etmitchell2022.github.io\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\evty2\\Desktop\\Side Projects\\cvSite\\etmitchell2022.github.io\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\evty2\\Desktop\\Side Projects\\cvSite\\etmitchell2022.github.io\\src\\pages\\index.js"))),
   "component---src-pages-regalia-js": hot(preferDefault(require("C:\\Users\\evty2\\Desktop\\Side Projects\\cvSite\\etmitchell2022.github.io\\src\\pages\\regalia.js"))),
